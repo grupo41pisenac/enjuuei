92d56aae8e652b26a27a0a3204db7a38f9775c77
vinigiu <viniciusgiuseppe1992@gmail.com>
2025-05-06 08:35:41 -0300

    add multer for file processing

--- enjuuei-api/src/modules/product/controller/product.controller.ts
+++ enjuuei-api/src/modules/product/controller/product.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { ListAllProductsDto } from '../dto/response/listAllProducts.dto';
@@ -13,6 +15,8 @@ import { CreateProductDto } from '../dto/request/createProduct.dto';
 import { UpdateProductDto } from '../dto/request/updateProduct.dto';
 import { SuccessDto } from 'src/core/dto/success.dto';
 import { IsPublic } from 'src/core/decorators/isPublic.decorator';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from 'src/core/config/multer.config';
 
 @Controller('product')
 export class ProductController {
@@ -31,8 +35,14 @@ export class ProductController {
   }
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto): Promise<SuccessDto> {
-    return this.productService.create(createProductDto);
+  @UseInterceptors(
+    FileFieldsInterceptor([{ name: 'images', maxCount: 5 }], multerConfig),
+  )
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFiles() files: { images?: Express.Multer.File[] },
+  ): Promise<SuccessDto> {
+    return this.productService.create(createProductDto, files.images || []);
   }
 
   @Patch(':id')
